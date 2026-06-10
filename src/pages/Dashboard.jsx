a0f672fa38829f59ec9f51bf847a2fc19c7967f8
dumiduhimsara <dumiduhimsara19@gmail.com>
2026-06-10 12:06:43 +0530

    Updated axios with credentials for secure cookies

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -309,7 +309,7 @@ const Dashboard = () => {
                                         </div>
                                     </div>
                                 ))}
-                            </div>
+                            </div>     
                         </div>
                     )}
 
